7e1683b63c1d45ad99147aad6a3d7b35f464ae93
Bruno Ricardo Wotzke <49734747+BrunoRW@users.noreply.github.com>
2023-05-06 23:43:18 -0300

    Add files via upload

---
+++ Tela1/app/telaC.js
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+
+export default function TelaC() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.main}>
+        <Text style={styles.title}>Tela C</Text>
+        <Link style={styles.link} href="/">Ir para Index</Link>
+
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 24,
+  },
+  main: {
+    flex: 1,
+    justifyContent: "center",
+    maxWidth: 960,
+    marginHorizontal: "auto",
+    alignItems: "center"
+  },
+  title: {
+    fontSize: 64,
+    fontWeight: "bold",
+  },
+  link: {
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: "black",
+    padding: 5,
+    borderRadius: 5,
+    width: "fit-content"
+  },
+});
